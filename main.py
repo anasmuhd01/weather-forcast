0a4af7843bf965efdbb4c9897cdf5b9ac1c3cf90
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-20 12:38:03 +0530

    try method added with keyerror: error 'when not valid place added in the place input'

--- main.py
+++ main.py
@@ -9,22 +9,24 @@ option = st.selectbox("select data to view", ('temperature', 'sky'))
 st.subheader(f"{option} for the next {forecast_days} days {place}")
 
 
+try:
+    if place:
+        filtered_data = get_date(place, forecast_days)
+        if option == "temperature":
+            st.caption("temperature in Celsius")
+            temperature = [dict["main"]["temp"] for dict in filtered_data]
+            dates = [dict["dt_txt"]for dict in filtered_data]
 
-if place:
-    filtered_data = get_date(place, forecast_days)
-    if option == "temperature":
-        st.caption("temperature in Celsius")
-        temperature = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"]for dict in filtered_data]
+            fig = px.line(x=dates, y=temperature, labels={"x": "date","y": "temperature"})
+            st.plotly_chart(fig)
 
-        fig = px.line(x=dates, y=temperature, labels={"x": "date","y": "temperature"})
-        st.plotly_chart(fig)
+        if option == "sky":
+            image = {"Clear":"images/clear.png", "Rain": "images/rain.png"
+                     , "Clouds":"images/cloud.png", "Snow": "images/snow.png"}
 
-    if option == "sky":
-        image = {"Clear":"images/clear.png", "Rain": "images/rain.png"
-                 , "Clouds":"images/cloud.png", "Snow": "images/snow.png"}
+            sky_conditon = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [image[condition] for condition in sky_conditon]
 
-        sky_conditon = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_path = [image[condition] for condition in sky_conditon]
-
-        st.image(image_path,width=115)
+            st.image(image_path,width=115)
+except KeyError:
+    st.info(f"there is no place named {place}. please enter valid place")
